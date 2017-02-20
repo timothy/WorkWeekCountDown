977436415a9e4ebc41e81d1fddfb46542bb5dd9f
timothy <tabradford@liberty.edu>
2017-02-20 17:42:00 -0500

    Added all days to view and start to and functionality.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,8 @@ import {TotalTime} from "./types";
 //import {Validate} from "../../classes/NumValidator";
 import {ConvertTime} from "../../classes/ConvertTime";
 
+enum T { hr, min }
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -19,6 +21,7 @@ export class HomePage {
   time: TotalTime = {decimal: 40, hours: 40, min: 0};
 
   constructor(public navCtrl: NavController) {
+    console.log(navCtrl);
     for (let i = 0; i < this.week; i++) {
       this.days.push({day: this.DOW[i], hm: 0, decimalTime: 0, index: i});
       this.oldTime.push(0);
@@ -29,16 +32,16 @@ export class HomePage {
    * This will calculate decimal time and hr/min time based on decimal input
    * and will update all other field to reflect this new time
    */
-  calcDecTime(index) {
-      //clear start and end times... does not make sense to keep them user manually inputs time amount
-      this.days[index].endDate = null;
-      this.days[index].startDate = null;
+  calcDecTime(index: number) {
+    //clear start and end times... does not make sense to keep them user manually inputs time amount
+    this.days[index].endDate = null;
+    this.days[index].startDate = null;
 
-      console.log(this.days[index].decimalTime);
-      console.log("this.days[index].decimalTime");
+    console.log(this.days[index].decimalTime);
+    console.log("this.days[index].decimalTime");
 
-      //Calculate end totals with new subtracted amount
-      //this.calcEndTotals(index);
+    //Calculate end totals with new subtracted amount
+    //this.calcEndTotals(index);
   }
 
   /**
@@ -46,7 +49,7 @@ export class HomePage {
    * and will update all other field to reflect this new time
    * @param index the index of the day array that is to be edited
    */
-  CalcTime(index) {
+  CalcTime(index: number) {
     //clear start and end times... does not make sense to keep them user manually inputs time amount
     this.days[index].endDate = null;
     this.days[index].startDate = null;
@@ -64,22 +67,51 @@ export class HomePage {
    * and will update all other field to reflect this new time
    * @param index the index of the day array that is to be edited
    */
-  calcStartEndTime(index) {
-    console.log(this.days[index].startDate);
-    console.log(this.days[index].endDate);
-    console.log("Start and End Dates");
-    //make sure there is information in both "start" and "end" sections before trying to work with it.
-    if (this.days[index].endDate && this.days[index].startDate && this.days[index].endDate.getTime() > this.days[index].startDate.getTime()) {
-      console.log("Start and End Dates are Valid!!!");
-      //find out how many milliseconds are between start and end times
-      //let result: number = this.days[index].endDate.getTime() - this.days[index].startDate.getTime();
+  calcStartEndTime(index: number) {
+    let totalHR: any;
+    let totalMN: any;
+    let start: number[];
+    let end: number[];
 
-      //update all other fields - based on new start and end times
-      //this.days[index].decimalTime = ConvertTime.MiliSec2Dec(result);
-      //this.days[index].hours = ConvertTime.Dec2Hour(this.days[index].decimalTime);
-      //this.days[index].min = ConvertTime.Dec2Min(this.days[index].decimalTime);
+    if (this.days[index].startDate && this.days[index].endDate) {
+      start = this.days[index].startDate.split(":");
+      end = this.days[index].endDate.split(":");
 
-      //this.calcEndTotals(index);
+      console.log(start);
+      console.log(end);
+      //if start time is less then end time
+      if (start[T.hr] < end[T.hr] || (start[T.hr] === end[T.hr] && start[T.min] < end[T.min])) {
+        console.log("++++++++++++++++++++++++++Validation***************************");
+
+        //if start min is more then end min then one hour needs to be subtracted from end
+        if (end[T.hr] > start[T.hr] && end[T.min] < start[T.min]) {
+          totalHR = (end[T.hr] - 1) - start[T.hr];
+          totalMN = (end[T.min] + 60) - start[T.min];
+        } else {
+          totalHR = end[T.hr] - start[T.hr];
+          totalMN = end[T.min] - start[T.min];
+        }
+
+        console.log(totalHR + ':' + totalMN);
+        //needs to be 2 digits
+        if (totalHR.toString().length === 1) {
+          totalHR = 0 + totalHR.toString();
+        }
+        //needs to be 2 digits
+        if (totalMN.toString().length === 1) {
+          totalMN = 0 + totalMN.toString();
+        }
+
+        //calc all
+        console.log(totalHR + ':' + totalMN);
+        this.days[index].hm = totalHR + ':' + totalMN;
+        this.days[index].decimalTime = ConvertTime.HourMin2Dec(Number(totalHR),Number(totalMN));
+        //TODO: Calc end totals
+
+      } else {//TODO: give user notification
+        this.days[index].endDate = null;
+        this.days[index].startDate = null;
+      }
     }
   }
 
@@ -88,7 +120,7 @@ export class HomePage {
    * Calculate end totals. If there is a previous amount then overwrite it with new amount
    * @param index the index of the day array that is to be edited
    */
-  calcEndTotals(index) {
+  calcEndTotals(index: number) {
     if (this.oldTime[index]) {
       this.time.decimal += this.oldTime[index];
       this.time.hours = ConvertTime.Dec2Hour(this.time.decimal);
